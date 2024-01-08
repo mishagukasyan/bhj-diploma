6cd6c9541d758f8748720aa2232b9703ec74698b
gukasyan <mishagukasyan@mail.ru>
2024-01-08 21:59:20 +0700

    change

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -1,18 +1,17 @@
 'use strict';
 
-class LoginForm extends AsyncForm {
+class RegisterForm extends AsyncForm {
 
   onSubmit( options ) {
-    User.login( options, response => {
-      if ( response && response.success && response.user ) {
-        User.setCurrent(response.user);
+    User.register( options, response => {
+      if ( response.success && response.user && response.user ) {
+        User.setCurrent( response.user );
         super.resetFormData();
-        App.setState('user-logged');
-        App.getModal('login').close();
+        App.setState( 'user-logged' );
+        App.getModal( 'register' ).close();
       } else {
         console.error();
       }
     });
   };
-
 }
